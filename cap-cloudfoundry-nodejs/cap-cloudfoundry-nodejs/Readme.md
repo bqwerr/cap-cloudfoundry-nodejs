984c2ca17fe8d414ac6dc6eaf257df00081bd6e3
Srujan <tummasrujan@gmail.com>
2024-03-31 19:26:41 +0530

    Initial App with Roles

---
+++ cap-cloudfoundry-nodejs/cap-cloudfoundry-nodejs/Readme.md
@@ -0,0 +1,7 @@
+```bash
+Install UI5 - npm install --global @ui5/cli
+Install CDS - npm install --global @sap/cds-dk
+Install Extension Pack - https://marketplace.visualstudio.com/items?itemName=SAPSE.sap-ux-fiori-tools-extension-pack
+Install Yeoman - npm install --global yo
+
+```
---
+++ cap-cloudfoundry-nodejs/cap-cloudfoundry-nodejs/app/app/project1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'ns/project1/test/integration/FirstJourney',
+		'ns/project1/test/integration/pages/RisksList',
+		'ns/project1/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('ns/project1') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
